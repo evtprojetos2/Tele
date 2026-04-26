62d24064afb79565ca0fa8cbe20de1394392f65c
evtprojetos2 <78711411+evtprojetos2@users.noreply.github.com>
2026-04-26 17:29:24 -0300

    Update gerar_pix.js

--- api/gerar_pix.js
+++ api/gerar_pix.js
@@ -6,8 +6,13 @@ export default async function handler(req, res) {
     if (req.method === 'OPTIONS') return res.status(200).end();
 
     try {
-        const { preco, descricao, email } = req.body || {};
-        const ACCESS_TOKEN = "APP_USR-8126974382900936-122403-60778a71eea0559684f430ac912cc5dd-189761504";
+        const body = req.body || {};
+        
+        // O SEU TOKEN (Recebe a Mensalidade do SaaS)
+        const TOKEN_DONO = "APP_USR-8126974382900936-122403-60778a71eea0559684f430ac912cc5dd-189761504";
+        
+        // Se vier um token do aplicativo cliente, usa o dele. Se não, usa o seu.
+        const ACCESS_TOKEN = body.token_loja || TOKEN_DONO;
 
         const response = await fetch("https://api.mercadopago.com/v1/payments", {
             method: "POST",
@@ -17,10 +22,10 @@ export default async function handler(req, res) {
                 "X-Idempotency-Key": Math.random().toString(36).substring(2)
             },
             body: JSON.stringify({
-                transaction_amount: Number(preco) || 0,
-                description: descricao || "Agendamento Barbearia",
+                transaction_amount: Number(body.preco) || 0,
+                description: body.descricao || "Pagamento",
                 payment_method_id: "pix",
-                payer: { email: email || "cliente@barbearia.com" }
+                payer: { email: body.email || "cliente@app.com" }
             })
         });
         
